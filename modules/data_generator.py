2c7803f7d622042c64876a7ca5edc0a1456ad201
armandolezama <armando.lezama.psic@gmail.com>
2023-09-27 01:03:46 -0600

    feat(py): added first example data sets

--- modules/data_generator.py
+++ modules/data_generator.py
@@ -1,3 +1,11 @@
+import numpy as np
+
 class data_generator:
   def __init__(self):
+    self.data_1 = {
+      'x': np.array([5, 15, 25, 35, 45, 55]).reshape((-1, 1)),
+      'y': np.array([5, 20, 14, 32, 22, 38]),
+      'x_new': np.arange(5).reshape((-1, 1)),
+    }
     print(self)
+    
