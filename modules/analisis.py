b131820c6d6009a026b5bb4b8477f5a0fa69eb04
armandolezama <armando.lezama.psic@gmail.com>
2023-09-29 12:00:06 -0600

    feat(py): added regression functions

--- modules/analisis.py
+++ modules/analisis.py
@@ -7,14 +7,14 @@ class linear_regression_analyzer:
   def __init__(self):
     print(self)
   
-  def simple_linear_regression(self, x_vars, y_var):
+  def simple_linear_regression(self, x_var, y_var):
     model = LinearRegression()
 
-    model.fit(x_vars, y_var)
+    model.fit(x_var, y_var)
 
-    r_sq = model.score(x_vars, y_var)
+    r_sq = model.score(x_var, y_var)
 
-    y_pred = model.predict(x_vars)
+    y_pred = model.predict(x_var)
 
     # y_pred = model.intercept_ + model.coef_ * x
     
@@ -30,4 +30,54 @@ class linear_regression_analyzer:
       'slope': model.coef_,
       'determination_coeff': r_sq,
       'predicted_response': y_pred,
+    }
+  
+  def multi_linear_regression(self, x_vars, y_var):
+    model = LinearRegression().fit(x_vars, y_var)
+
+    r_sq = model.score(x_vars, y_var)
+
+    y_pred = model.predict(x_vars)
+
+    ## y_pred = model.intercept_ + np.sum(model.coef_ * x, axis=1)
+
+    return {
+      'model': model,
+      'intercept': model.intercept_,
+      'slope': model.coef_,
+      'determination_coeff': r_sq,
+      'predicted_response': y_pred,
+    }
+  
+  def polynomial_regression(self, x_var, y_var, include_bias = False, fit_intercept = False):
+    x_ = PolynomialFeatures(degree=2, include_bias=include_bias).fit_transform(x_var)
+
+    model = LinearRegression(fit_intercept=fit_intercept).fit(x_, y_var)
+
+    r_sq = model.score(x_, y_var)
+
+    y_pred = model.predict(x_)
+
+    return {
+      'model': model,
+      'intercept': model.intercept_,
+      'coefficients': model.coef_,
+      'determination_coeff': r_sq,
+      'predicted_response': y_pred,
+    }
+
+  def detailed_OLS_linear_regression(slef, x_var, y_var):
+    x = sm.add_constant(x)
+    model = sm.OLS(y_var, x)
+    results = model.fit()
+    predicted = results.predict(x)
+
+    return {
+      'results': results,
+      'determination_coeff': results.rsquared,
+      'adj_determination_coeff': results.rsquared_adj,
+      'regression_coefficients': results.params,
+      ## Predicted and fitted values are the same
+      'fitted_values': results.fittedvalues,
+      'predicted': predicted,
     }
